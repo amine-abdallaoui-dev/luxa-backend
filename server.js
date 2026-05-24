7cc0f3c7d4600feff7074065a7de1706258c0e7c
amine-abdallaoui-dev <thekasper2030@gmail.com>
2026-05-24 20:41:52 +0200

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const { Server } = require("socket.io");
 const { dbConnect } = require("./utils/dbConnect");
 
 app.use(cors({
-  origin: ["http://localhost:5173","http://localhost:5174","http://localhost:5175"],
+  origin: ["https://luxa-admin-dashboard.vercel.app","https://luxa-seller-dashboard.vercel.app","https://luxa-frontend.vercel.app"],
   credentials : true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With", "Accept"]
